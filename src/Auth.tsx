87e3720947fe77f4a7145126388df3a4148dd750
Skuablou <markclump@gmail.com>
2026-03-28 12:26:09 +0100

    Add queryParams to Google sign-in options

--- src/Auth.tsx
+++ src/Auth.tsx
@@ -32,7 +32,7 @@ export default function Auth() {
   const handleGoogle = async () => {
     await supabase.auth.signInWithOAuth({
       provider: 'google',
-      options: { redirectTo: window.location.origin }
+          options: { redirectTo: window.location.origin, queryParams: { prompt: 'select_account' } }
     })
   }
 
